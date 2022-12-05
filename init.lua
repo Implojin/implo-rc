07b3e5751f4f6b08b6dc7b67068d75907593e3ea
Implojin <implosionwow@gmail.com>
2022-12-05 17:34:09 -0600

    handle malmutate in lof
    I've added a basic handler for lof checks here, but it's not clear
    to me yet whether crawl's spells.path() clua bind permits path testing
    with monster spells. LOF checking for "Malmutate" doesn't seem to work
    at present, so I'm defaulting the LOF checks to use "Magic Dart".

--- init.lua
+++ init.lua
@@ -36,11 +36,42 @@ local threat_table = {}
 local DEBUG_MONS_STATUS = false
 local DEBUG_MONS_DESC = false
 local DEBUG_MONS_TARGET_DESC = false
+local DEBUG_LOF_PATH = false
 
 local function all_true(table)
     for _,v in ipairs(table) do
         if v == nil then return false end
         if not (v == true) then return false end
+    end
+    return true
+end
+
+local function same_coords(c1x, c1y, c2x, c2y)
+    if c1x == c2x and c1y == c2y then return true end
+    return false
+end
+
+-- test if we have line of fire to this mons
+-- @param mons mons
+-- @param[opt="Magic Dart"] string optional, test LOF using this spell name
+-- @return bool false if LOF is blocked or the spell path to mons is disjoint,
+--              true if spell has clear LOF to mons from the player's position
+function check_lof(mons, spell)
+    local path = nil
+    spell = spell or "Magic Dart"
+    path = spells.path(spell, mons:pos())
+    if path == nil then return false end
+
+    if DEBUG_LOF_PATH == true then
+        crawl.mpr(spell .. " path to " .. mons:name() .. ": ")
+        for _,vertex in ipairs(path) do
+            crawl.mpr(vertex[1] .. "," .. vertex[2])
+        end
+    end
+
+    if not same_coords(path[#path][1], path[#path][2], mons:pos()) then
+        if DEBUG_LOF_PATH == true then crawl.mpr("LOF blocked to " .. mons:name() .. "?") end
+        return false
     end
     return true
 end
@@ -65,6 +96,8 @@ local status = {
         if DEBUG_MONS_STATUS == true then crawl.mpr(mons:name() .. " status: " .. mons:status() ) end
         if DEBUG_MONS_DESC == true then crawl.mpr(mons:name() .. " desc: " .. mons:desc(true) ) end
         if DEBUG_MONS_TARGET_DESC == true then crawl.mpr(mons:name() .. " target_desc: " .. mons:target_desc() ) end
+        if DEBUG_LOF_PATH == true then check_lof(mons, "Magic Dart") end
+
 
 -- begin danger table
         -- TODO: Fill out the remaining danger conditions:
@@ -102,10 +135,14 @@ local status = {
         {conditions = {check_tdesc(mons, "wand of paralysis"), you.willpower() < 3},
              reason = "Wand of Paralysis and low Will"} ,
         -- comparing "distort" instead of "distortion" works against Rift, randarts, and panlord "distorting touch"
+        -- TODO: check to see if this catches dancing weapons?
         {conditions = {check_tdesc(mons, "[dD]istort"), you.branch() ~= "Zig"},
              reason = "Distortion weapon!"} ,
         {conditions = {check(mons, "Malmutate"), you.branch() ~= "Zig"},
-             reason = "Malmutator in LOS"} , }
+             reason = "Malmutator in LOS"} ,
+        -- XXX: I'd prefer to check lof against "Malmutate" here, but it seems like spells.path() only works with player spells?
+        {conditions = {check(mons, "Malmutate"), check_lof(mons), you.branch() ~= "Zig"},
+             reason = "Malmutator in LOF!"} , }
 
         for _,threat in ipairs(danger_table) do
             if all_true(threat.conditions) then
@@ -124,8 +161,8 @@ local status = {
     -- (maybe allow some extreme threats to pop every turn? e.g. paralyse with low Will)
     _warn = function ()
         for _,threat in ipairs(threat_table) do
-            crawl.formatted_mpr("<lightred>Danger: " .. threat[1]:name() .. "</lightred>")
-            crawl.formatted_mpr("<lightblue>Reason: " .. tostring(threat[2]) .. "</lightblue>")
+            crawl.formatted_mpr("<lightred>Danger: " .. threat[1]:name() .. "</lightred> || " ..
+                                "<lightblue>Reason: " .. tostring(threat[2]) .. "</lightblue>")
             crawl.more()
         end
     end,
