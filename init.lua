b5297503144a1912b5bf69bf31263c2a5f0048b8
Implojin <implosionwow@gmail.com>
2023-05-02 18:11:44 -0500

    update use_animations for 0.30 compatibility
    This option syntax was changed on the Crawl side, we need to blank it instead
    of setting it to false.

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ rest_delay = -1
 # try to increase flash_screen_message visibility with use_animations == false
 view_delay = 0
 show_travel_trail = true
-use_animations = false
+use_animations = 
 
 # megazigging
 show_more = false
