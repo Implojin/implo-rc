Implojin/implo-rc

commit e57ad287605b3b78721ca85551944fe58b02874d

Fix an autoexplore + autopickup loop
Prior to this commit, the script's maybe_reissue_autoexplore conditions
could send the game into a loop if autoexplore ended on a tile adjacent
to a closed door and containing an item marked for autopickup, while the
player's inventory was full.

This commit is a small fix that forces the script to stop reissuing
autoexplore in this particular case.

Our maybe_reissue_autoexplore logic is pretty bad, though, and there are
probably similar unhandled errors lurking here, where autoexplore
*should* end next to a door but instead sends itself into a loop.

Ultimately, this reissue autoexplore logic needs a rewrite at some point,
but that can happen in another commit.

init.lua
@@ -43,6 +43,8 @@ tile_show_threat_levels = tough, nasty, unusual
 
 cloud_status = true
 
+macros += M 'o' ===do_autoexplore
+
 # player tile things
 tile_weapon_offsets = 0,0
 tile_shield_offsets = 0,0
@@ -532,6 +534,7 @@ end
 -- Crawl doesn't send any kind of message when it stops autoexplore adjacent to a closed door.
 -- As a result, we have to guard against infinite loops here.
 local reissue_autoexplore_needed = false
+local force_autoexplore_stop = false
 local loop_guard = 0
 local done_exploring = false
 function maybe_reissue_autoexplore()
@@ -556,6 +559,12 @@ function maybe_reissue_autoexplore()
     end
     loop_guard = 0
     return false
+end
+
+function do_autoexplore()
+    reissue_autoexplore_needed = false
+    force_autoexplore_stop = false
+    crawl.sendkeys('o')
 end
 
 -- Returns true if this mons should be treated as threatening for autotravel, warning, and combat purposes.
@@ -1002,8 +1011,9 @@ function ch_stop_running(runmode)
     -- These conditions test if our autoexplore was interrupted early due to travel_open_doors = avoid.
     -- XXX: This check is imperfect, and is likely to send the script into infinite autoexplore loops.
     -- I've added an assert and some harder checks to maybe_reissue_autoexplore(), but this could still be improved.
-    if runmode == "explore_greedy" and check_adjacent_feat("closed_door", true) and you.feel_safe() then
-        reissue_autoexplore_needed = true
+    if runmode == "explore_greedy" and check_adjacent_feat("closed_door", true)
+       and you.feel_safe() and not force_autoexplore_stop then
+           reissue_autoexplore_needed = true
     end
 end
 
@@ -1015,11 +1025,16 @@ function ch_start_running(runmode)
     end
 end
 
--- skip chain lightning and shatter ally prompts when megazigging with death channel
 function c_answer_prompt(prompt)
+    -- skip chain lightning and shatter ally prompts when megazigging with death channel
     if you.branch() == "Zig" and you.status("death channelling") == true then
         if prompt:find("Chain Lightning might hit") then return true end
         if prompt:find("Really attack near") then return true end
+    end
+    -- try to prevent the script from becoming stuck in a failed autopickup -> reissue autoexplore loop,
+    -- if it fails to pick something up while adjacent to a closed door
+    if prompt:find("Could not pick up") then
+        force_autoexplore_stop = true
     end
 end
 }

commit bf9fd3a83a71a28b8e716b50cbbbc5e4d967568e

Further reissue_autoexplore improvements
Here we guard against another infinite loop case, and fix the
reissue_autoexplore one-tap behavior, which was broken in commit
e57ad28760.

Prior to this commit, the script was looping whenever autoexplore
stopped due to inaccessible terrain, while adjacent to a door.

init.lua
@@ -562,7 +562,7 @@ function maybe_reissue_autoexplore()
 end
 
 function do_autoexplore()
-    reissue_autoexplore_needed = false
+    crawl.setopt("travel_open_doors = avoid")
     force_autoexplore_stop = false
     crawl.sendkeys('o')
 end
@@ -963,9 +963,8 @@ local message_log_threat_table = {
 -- TODO: check messages for whether the player was shafted / teleported, and if so, stop reissuing autoexplore?
 -- I don't want the script blindly re-issuing actions if the floor situation has changed.
 function c_message(text, channel)
-    if string.find(text, "Done exploring.") then
-        done_exploring = true
-    end
+    if string.find(text, "Done exploring.") then done_exploring = true end
+    if string.find(text, "Partly explored") then done_exploring = true end
 
     for _,threat in ipairs(message_log_threat_table) do
         if string.find(text, threat.conditions) then
@@ -1012,7 +1011,7 @@ function ch_stop_running(runmode)
     -- XXX: This check is imperfect, and is likely to send the script into infinite autoexplore loops.
     -- I've added an assert and some harder checks to maybe_reissue_autoexplore(), but this could still be improved.
     if runmode == "explore_greedy" and check_adjacent_feat("closed_door", true)
-       and you.feel_safe() and not force_autoexplore_stop then
+       and you.feel_safe() and force_autoexplore_stop == false then
            reissue_autoexplore_needed = true
     end
 end